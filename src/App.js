8b89d84b24274b225316769d9e1cc0530e58883c
김중우 <tirkffus@naver.com>
2025-05-19 21:06:06 +0900

    ex3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import R003_ImportComponent from './react/part1/R003_ImportComponent.js';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
+      <R003_ImportComponent></R003_ImportComponent>
     </div>
   );
 }
