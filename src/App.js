245f4dd0837273b748b1654eb7edd931deb2ca38
김중우 <tirkffus@naver.com>
2025-05-20 00:18:23 +0900

    ex4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import R003_ImportComponent from './react/part1/R003_ImportComponent.js';
+import R004_LifecycleEx from './react/part1/R004_LifecycleEx.js';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
-      <R003_ImportComponent></R003_ImportComponent>
+      <R004_LifecycleEx></R004_LifecycleEx>
     </div>
   );
 }
